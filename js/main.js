dc8c7914d2b8f6e982bb0d18e50f9d89154e494c
Paola Quispe <paola.qmelendez@gmail.com>
2016-11-14 10:24:11 -0500

    agregando cambios

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-	var container = document.getElementById('container');
-	setTimeout(function() {
-		container.classList.add('cerrar');
-	  	document.body.style.overflowY= "visible";
-	}, 6000);
+$(window).ready(function() {
+	$(".loading").fadeOut("slow");
+});
+
